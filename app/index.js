ca5edd0cf54cc1d077db7a8fb9d7a45129cc3d29
j8298c <juliocmojica@gmail.com>
2018-03-13 22:04:34 -0400

    post new chain

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Blockchain = require('../blockchain');
+const bodyParser = require('body-parser');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
@@ -7,8 +8,17 @@ const app = express();
 
 const bc = new Blockchain();
 
+app.use(bodyParser.json());
+
+
 app.get('/blocks', (req, res) => {
 	res.json(bc.chain);
+});
+
+app.post('/mine', (req, res) => {
+	const block = bc.addBlock(req.body.data);
+	console.log(`new block added: ${block.toString()}`);
+	res.redirect('/blocks');
 });
 
 app.listen(HTTP_PORT, () => {
