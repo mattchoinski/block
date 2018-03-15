78da3f8d2f60f86c4093f58f2ca7453a892e62f9
j8298c <juliocmojica@gmail.com>
2018-03-15 04:49:02 -0400

    peer to peer serve multi conns

--- app/index.js
+++ app/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const Blockchain = require('../blockchain');
 const bodyParser = require('body-parser');
+const PeerToPeerServer = require('./peertopeerserver');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 
 const bc = new Blockchain();
+const p2pServer = new PeerToPeerServer(bc);
 
 app.use(bodyParser.json());
 
@@ -24,3 +26,5 @@ app.post('/mine', (req, res) => {
 app.listen(HTTP_PORT, () => {
 	console.log(`listening on port ${HTTP_PORT}`);
 });
+
+p2pServer.listen();
